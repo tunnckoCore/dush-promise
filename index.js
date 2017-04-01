6f84ed3bf49a0748aef6f5dbc02a9a91a2994a8a
tunnckoCore <mameto_100@mail.bg>
2017-04-01 10:00:46 +0300

    fix(docs): generate readme and api docs

--- index.js
+++ index.js
@@ -9,22 +9,142 @@
 
 var Deferred = require('native-promise-deferred')
 
+/**
+ * > Adds a Promise methods such as `.resolve`, `.reject`
+ * `.then` and `.catch` to your [dush][] application. Useful
+ * from inside plugins. This plugin also emits `error` event
+ * when `app.reject` is used.
+ *
+ * **Example**
+ *
+ * ```js
+ * var dush = require('dush')
+ * var promise = require('dush-promise')
+ *
+ * var app = dush().use(promise())
+ *
+ * console.log(app.then)
+ * console.log(app.catch)
+ * console.log(app.reject)
+ * console.log(app.resolve)
+ * ```
+ *
+ * @param  {Object} `opts` optional, passed directly to [native-promise-deferred][]
+ * @return {Function} a plugin function that should be passed to `.use` method of [dush][]
+ * @api public
+ */
+
 module.exports = function dushPromise (opts) {
   return function dushPromise_ (app) {
     var promise = Deferred(opts)
 
-    app.then = function (fn, rej) {
-      return promise.then(fn, rej)
+    /**
+     * > Handle resolved promise with `onresolved` or rejected promise
+     * with `onrejected`. It is as any usual Promise `.then` method.
+     *
+     * **Example**
+     *
+     * ```js
+     * app.then((res) => {
+     *   console.log(res) // => 123
+     * })
+     * app.resolve(123)
+     *
+     * // or handle rejected promise
+     * app.then(null, (er) => {
+     *   console.log('err!', er) // => Error: foo bar
+     * })
+     * app.reject(new Error('foo bar'))
+     * ```
+     *
+     * @name   .then
+     * @param  {Function} `onresolved`
+     * @param  {Function} `onrejected`
+     * @return {Promise}
+     * @api public
+     */
+
+    app.then = function (onresolved, onrejected) {
+      return promise.then(onresolved, onrejected)
     }
-    app.catch = function (fn) {
-      return promise.catch(fn)
+
+    /**
+     * > Catch a rejected promise error. This method is mirror
+     * of any usual promise `.catch` method.
+     *
+     * **Example**
+     *
+     * ```js
+     * app.on('error', (err) => {
+     *   console.log('er!', err) // => Error: sad err
+     * })
+     * app.catch((err) => {
+     *   console.log('oops, error!', err) // => Error: sad err
+     * })
+     *
+     * app.reject(new Error('sad err'))
+     * ```
+     *
+     * @name   .catch
+     * @param  {Function} `onrejected`
+     * @return {Promise}
+     * @api public
+     */
+
+    app.catch = function (onrejected) {
+      return promise.catch(onrejected)
     }
+
+    /**
+     * > As any usual `Promise.resolve` method.
+     *
+     * **Example**
+     *
+     * ```js
+     * app.use((app) => {
+     *   app.foo = () => {
+     *     return app.resolve(1222)
+     *   }
+     * })
+     *
+     * const promise = app.foo()
+     * promise.then((val) => {
+     *   console.log('res:', val) // => 1222
+     * })
+     * ```
+     *
+     * @name   .resolve
+     * @param  {any} `val`
+     * @return {Promise}
+     * @api public
+     */
+
     app.resolve = function resolve (val) {
       return promise.resolve(val)
     }
-    app.reject = function (er) {
-      return promise.reject(er)
+
+    /**
+     * > As any usual `Promise.reject` method.
+     *
+     * **Example**
+     *
+     * ```js
+     * app.on('error', (err) => {
+     *   console.log('some error:', err) // => Error: quxie
+     * })
+     * app.reject(new Error('quxie'))
+     * ```
+     *
+     * @name   .reject
+     * @param  {Error} `err`
+     * @return {Promise}
+     * @api public
+     */
+
+    app.reject = function (err) {
+      return promise.reject(err)
     }
+
     app.catch(function (er) {
       app.emit('error', er)
     })
